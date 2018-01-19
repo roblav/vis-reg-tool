19dcdd4f6cd94bbe9800b891a86bf55e4a0760fe
robert.lavender <robert.lavender@digital.hmrc.gov.uk>
2018-01-19 08:53:52 +0000

    First draft of generating diff images.

--- index.js
+++ index.js
@@ -1,30 +1,53 @@
 
 const resemble = require('node-resemble-js')
 const fs = require('fs-extra')
+const array = require('lodash/array');
+const empty = require('is-empty')
 
-var file = "./images/reference/Change received - Tax Credits Service.png"
-var file2 = "./images/test/Change received - Tax Credits Service.png"
+const listImages = require('./list-images')
 
-// var diff = resemble(file).compareTo(file2).ignoreColors().onComplete(function(data){
-//   console.log(data)
-//   /*
-//   {
-//     misMatchPercentage : 100, // %
-//     isSameDimensions: true, // or false
-//     dimensionDifference: { width: 0, height: -1 }, // defined if dimensions are not the same
-//     getImageDataUrl: function(){}
-//   }
-//   */
-// });
+var file = './images/reference/Change received - Tax Credits Service.png'
+var file2 = './images/test/Change received - Tax Credits Service.png'
 
-var diff2 = resemble(file).compareTo(file2).ignoreNothing().onComplete(function(data) {
-  const diffImagePath = './images/diff/' + 'diff.jpg'
-  // var diffImage = data.getImageDataUrl().replace(/^data:image\/png;base64,/,"");
-  var diffImage = data.getDiffImageAsJPEG(85)
-  // Write the diff file to disk.
-  fs.writeFile(diffImagePath, diffImage, function(err) {
-    if (err) throw err;
 
-    console.log('Your diff has been saved to %s', diffImagePath);
-  });
-});
+const imagePath = './images'
+const refImagePath = '/reference/'
+const testImagePath = '/test/'
+
+Promise.all([
+  listImages(imagePath + refImagePath),
+  listImages(imagePath + testImagePath)
+])
+.then(
+  (data) => {
+      var match = array.difference(data[0], data[1])
+      if (empty(match)) {
+        console.log("It's a match")
+        vrtCreateDiff(data[0])
+      } else {
+        console.log("Your reference and test images do not match. Mis-matched files: " + match)
+      }
+    }
+)
+.catch((err) => console.log(err))
+
+function vrtCreateDiff (imgList) {
+  //var testImages = listImages(imagePath + testImagePath)
+
+  //console.log(imgList)
+  imgList.forEach( function (img) {
+    var refImg = imagePath + refImagePath + img
+    var testImg = imagePath + testImagePath + img
+    var diff = resemble(refImg).compareTo(testImg).ignoreNothing().onComplete(function(data) {
+      const diffImagePath = './images/diff/' + img +'.jpg'
+      var diffImage = data.getDiffImageAsJPEG(85)
+      // Write the diff file to disk.
+      fs.writeFile(diffImagePath, diffImage, function(err) {
+        if (err) throw err;
+        console.log('Your diff has been saved to %s', diffImagePath);
+      });
+    });
+  })
+  
+}
+    
